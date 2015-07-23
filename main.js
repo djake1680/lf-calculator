ba6c29d5cd46d0a53bddd3d072cdc4532167a3ff
Darin Jacobson <djake1680@gmail.com>
2015-07-23 11:43:18 -0700

    1.0[complete] with decimal and divide/0 error

--- main.js
+++ main.js
@@ -4,14 +4,24 @@ var number_array = ["",""];
 var clear_screen;
 var my_operator;
 
-//Enters number of the button you clicked on, and keeps adding to it with new numbers
-function button_click(button_number){
-	var new_number = $(".calc_screen").val();
-	var current_input = new_number + button_number;
-	$(".calc_screen").val(current_input);
-	number_array[first_array_number]+=button_number;
-}
 
+$(document).ready(function(){
+	//Enters number of the button you clicked on, and keeps adding to it with new numbers
+$(".button_clk").click(function(){
+	console.log($(this).text());
+	var button_no = $(this).text();
+	var new_number = $(".calc_screen").val();
+	var current_input = new_number + button_no;
+	$(".calc_screen").val(current_input);
+	number_array[first_array_number]+=button_no;
+});
+
+//when hit =, passes the array items and operator to number_sum function
+$(".equals").click(function(){ 
+	console.log($(this).text());
+	number_sum(number_array[0], my_operator, number_array[1]);
+});
+});
 function clear_function(){
 	console.log('worked');
 	number_array = ["",""];
@@ -20,18 +30,13 @@ function clear_function(){
 }
 
 //put all numbers and operators in display
+
 function get_display(operator_new){
 	var first_number = $(".calc_screen").val();
 	$(".calc_screen").val(first_number + operator_new);//to clear the screen so number starts over
 	first_array_number = 1;
 	my_operator = operator_new;
 }
-//when = entered, use all 3 parameters for number_sum
-function number_equal(){
-	console.log(my_operator);
-	number_sum(number_array[0], my_operator, number_array[1]);
-}
-
 
 //function takes your two numbers and the operator
 function number_sum(number1, operator, number2){
@@ -66,38 +71,3 @@ function number_sum(number1, operator, number2){
 
 	}
 }
-
-/*$(document).ready(function(){
-
-//finds 2 numbers in the input field, and then passes them along with + to number_sum function
-$("#add_button").click(function(){
-	operator = "+";
-	//var num_enter_0 = $(".number_0").val();
-	//var num_enter_1 = $(".number_1").val();
-	var num_enter_0 = number_array[0];
-	var num_enter_1 = number_array[1];
-	console.log(number_enter_0);
-	number_sum(num_enter_0, operator, num_enter_1);
-});
-//finds 2 numbers in the input field, and then passes them along with - to number_sum function
-$("#sub_button").click(function(){
-	operator = "-";
-	var num_enter_0 = $(".number_0").val();
-	var num_enter_1 = $(".number_1").val();
-	number_sum(num_enter_0, operator, num_enter_1);
-});
-//finds 2 numbers in the input field, and then passes them along with * to number_sum function
-$("#multiply_button").click(function(){
-	operator = "*";
-	var num_enter_0 = $(".number_0").val();
-	var num_enter_1 = $(".number_1").val();
-	number_sum(num_enter_0, operator, num_enter_1);
-});
-//finds 2 numbers in the input field, and then passes them along with / to number_sum function
-$("#divide_button").click(function(){
-	operator = "/";
-	var num_enter_0 = $(".number_0").val();
-	var num_enter_1 = $(".number_1").val();
-	number_sum(num_enter_0, operator, num_enter_1);
-});
-});*/
