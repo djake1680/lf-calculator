ec387a924785389ab025c8854dfef65aaf37fe4a
Darin Jacobson <djake1680@gmail.com>
2015-07-23 17:59:14 -0700

    calculater 1.5[needs (-) button and clear (not A/C) to work

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var first_array_number = 0;
 var number_array = ["",""];
 var clear_screen;
 var my_operator;
-
+var current_input;
 
 $(document).ready(function(){
 	//Enters number of the button you clicked on, and keeps adding to it with new numbers
@@ -11,32 +11,35 @@ $(".button_clk").click(function(){
 	console.log($(this).text());
 	var button_no = $(this).text();
 	var new_number = $(".calc_screen").val();
-	var current_input = new_number + button_no;
+	current_input = new_number + button_no;
 	$(".calc_screen").val(current_input);
 	number_array[first_array_number]+=button_no;
 });
 
+
 //when hit =, passes the array items and operator to number_sum function
-$(".equals").click(function(){ 
-	console.log($(this).text());
+$(".equals").click(function(){
 	number_sum(number_array[0], my_operator, number_array[1]);
 });
-});
-function clear_function(){
-	console.log('worked');
+
+//to clear the screen when "CLEAR" is clicked
+$("#clear_screen").click(function(){
 	number_array = ["",""];
 	$(".calc_screen").val("");
 	first_array_number = 0;
-}
-
+});
 //put all numbers and operators in display
+	$(".operator_click").click(function(){
+		var clicked_operator = $(this).text();
+		console.log(clicked_operator);
+		var first_number = $(".calc_screen").val();
+		$(".calc_screen").val(first_number + clicked_operator);
+		first_array_number = 1;
+		my_operator = clicked_operator;
 
-function get_display(operator_new){
-	var first_number = $(".calc_screen").val();
-	$(".calc_screen").val(first_number + operator_new);//to clear the screen so number starts over
-	first_array_number = 1;
-	my_operator = operator_new;
-}
+	});
+
+});
 
 //function takes your two numbers and the operator
 function number_sum(number1, operator, number2){
